a2842fdf9b2eceddff128affa2bd40dc6877cb6a
Sam512 <33881069+Sam512@users.noreply.github.com>
2019-04-30 20:17:55 +0100

    Jekyll Hyde & General Aesthetic

--- src/app/jh/jh.component.ts
+++ src/app/jh/jh.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./jh.component.css']
 })
 export class JhComponent implements OnInit {
-
-  constructor() { }
-
+  chapterNo: number;
+  blue: boolean;
+  constructor() {}
   ngOnInit() {
+    this.blue = true;
   }
 
+  coverUp() {
+    const coverCloud = document.getElementById('cover-cloud');
+    coverCloud.classList.remove('triggered-back');
+    coverCloud.classList.add('triggered');
+  }
+
+  coverDown() {
+    const coverCloud = document.getElementById('cover-cloud');
+    coverCloud.classList.remove('triggered');
+    coverCloud.classList.add('triggered-back');
+  }
+
+  changeChapter() {
+    console.log('triggered');
+    if (this.blue) {
+      this.coverUp();
+      setTimeout(() => {
+        this.coverDown();
+        document
+          .getElementsByClassName('hero')[0]
+          .classList.remove('bg-moonstone');
+        this.blue = false;
+      }, 1500);
+    }
+  }
 }
