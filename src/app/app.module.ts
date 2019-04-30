c45c9d842550965e2f2fa1a33782c58baec53450
Sam512 <33881069+Sam512@users.noreply.github.com>
2019-04-30 20:00:36 +0100

    Added Jekyll Hyde, Home and Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { JhComponent } from './jh/jh.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    JhComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/jh/jh.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-jh',
+  templateUrl: './jh.component.html',
+  styleUrls: ['./jh.component.css']
+})
+export class JhComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
