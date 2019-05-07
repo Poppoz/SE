3467cb9491330cdfd06772e45b702417518597fb
Sam512 <33881069+Sam512@users.noreply.github.com>
2019-05-07 18:24:40 +0100

    Added Branding Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { JhComponent } from './jh/jh.component';
+import { BrandingComponent } from './branding/branding.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'jh', component: JhComponent }
+  { path: 'jh', component: JhComponent },
+  { path: 'branding', component: BrandingComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { JhComponent } from './jh/jh.component';
+import { BrandingComponent } from './branding/branding.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent,
-    JhComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HomeComponent, JhComponent, BrandingComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
